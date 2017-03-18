ba37c44d226d9128f3e3bb8687b07b45aacd2fcc
Andreas Litzell Ivarsson <andreas.ivarsson@istone.com>
2017-03-18 16:21:52 +0100

    Adding test for azure storage handler

---
+++ test/Azure.RestApi.Tests/AzureStorageHandlerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using Xunit;
+
+namespace Azure.RestApi.Tests
+{
+    public class AzureStorageHandlerTests
+    {
+        private AzureStorageHandler GetHandler()
+        {
+            return new AzureStorageHandler(new StorageAuthentication { AccountName = FakeData.ACCOUNTNAME, StorageKey = FakeData.STORAGEKEY });
+        }
+
+        [Fact]
+        public void GetCanonicalizedResource()
+        {
+            //Given
+            var expected = "/testaccount/";
+
+            //When
+            var actual = GetHandler().GetCanonicalizedResource(Models.StorageType.Queue, FakeData.GetBaseUrl(FakeData.QUEUE), FakeData.ACCOUNTNAME);
+
+            //Then
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetRequest()
+        {
+            //Given
+            var queue = "testqueue";
+
+            //When
+            var actual = GetHandler().GetRequest(Models.StorageType.Queue, HttpMethod.Get, $"{queue}/messages");
+
+            //Then
+            Assert.Equal(HttpMethod.Get, actual.Method);
+            Assert.Equal(new Uri("https://testaccount.queue.core.windows.net/testqueue/messages"), actual.RequestUri);
+            Assert.Equal("SharedKey", actual.Headers.Authorization.Scheme);
+        }
+    }
+}
---
+++ test/Azure.RestApi.Tests/FakeData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Azure.RestApi.Tests
+{
+    public static class FakeData
+    {
+        public const string QUEUE = "queue";
+        public const string TABLE = "table";
+        public const string BLOB = "blob";
+
+        public const string STORAGEKEY = "SSjwpaQxpuE39Ge1rtQSFG2d+SuyCdLfBavFFvWkgGuiJ3snVmHN7YNYsd1rQ6FVlco2GzCbfkwOplcrHJTgNA==";
+        public const string ACCOUNTNAME = "testaccount";
+
+        public static Uri GetBaseUrl(string containerType)
+        {
+            return new Uri($"https://{ACCOUNTNAME}.{containerType}.core.windows.net/");
+        }
+    }
+}
