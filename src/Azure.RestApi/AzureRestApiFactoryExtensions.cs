a5422ed347b13d4f7f463ebc5959af7204cf19ad
Andreas Litzell Ivarsson <andreas.ivarsson@creuna.se>
2018-11-21 06:56:49 +0100

    add registration extensions

---
+++ src/Azure.RestApi/AzureRestApiFactoryExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Azure.RestApi;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+
+namespace Microsoft.Extensions.Logging
+{
+	public static class AzureRestApiFactoryExtensions
+	{
+		public static IServiceCollection AddAzureRestApi(this IServiceCollection services, Action<AzureRestApiOptions> configure)
+		{
+			if (configure == null)
+			{
+				throw new ArgumentNullException(nameof(configure));
+			}
+
+			services.AddSingleton<IAzureStorageHandler, AzureStorageHandler>();
+			services.AddSingleton<IBlobHandler, BlobHandler>();
+			services.AddSingleton<IQueueHandler, QueueHandler>();
+			services.AddSingleton<ITableHandler, TableHandler>();
+			services.AddSingleton<IWebRequest, WebRequest>();
+
+			services.Configure(configure);
+
+			return services;
+		}
+	}
+}
